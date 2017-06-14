d4ce9a07e2203d16fc0f78f7a9674fa3391ec733
natedaag <natedaag@natedaag-Satellite-L755>
2017-06-14 09:47:13 -0600

    added a try/catch NumberFormatException

--- src/edu/cnm/deepdive/euclid/Gcd.java
+++ src/edu/cnm/deepdive/euclid/Gcd.java
@@ -8,17 +8,21 @@ package edu.cnm.deepdive.euclid;
  *
  */
 public class Gcd {
-	
-	public static void main(String[] args) {
-	
-int a = Integer.parseInt(args[0]);
-int b = Integer.parseInt(args[1]);
 
-	/**
-	 * @param args
-	 */
-	System.out.printf("a=%d, b=%d %n", a, b);
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+
+		try {
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+			System.out.printf("a=%d, b=%d %n", a, b);
+			// TODO Auto-generated method stub
+		} catch (NumberFormatException ex) {
+			ex.printStackTrace();
+			System.out.println("Your application requires" + " two numbers typed to the command line.");
+		}
+		/**
+		 * @param args
+		 */
 
 	}
 
