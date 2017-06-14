62370c7a2332943a26584b08663929f559e593ce
natedaag <natedaag@natedaag-Satellite-L755>
2017-06-14 10:45:22 -0600

    added second method, added result to main method

--- src/edu/cnm/deepdive/euclid/Gcd.java
+++ src/edu/cnm/deepdive/euclid/Gcd.java
@@ -14,19 +14,25 @@ public class Gcd {
 		try {
 			int a = Integer.parseInt(args[0]);
 			int b = Integer.parseInt(args[1]);
-			System.out.printf("a=%d, b=%d %n", a, b);
+			int result = gcd(a,b);
+			
+			System.out.printf("a=%d, b=%d, GCD=%d %n", a, b, result);
 			// TODO Auto-generated method stub
 		} catch (NumberFormatException ex) {
 			ex.printStackTrace();
-			System.out.println("Your application requires" + 
-		" two numbers typed to the command line.");
+			System.out.println("Your application requires" + " two numbers typed to the command line.");
 		} catch (ArrayIndexOutOfBoundsException ex) {
 			System.out.println("Your application requires two numbers typed to the command line.");
-		}
+		}  
+
 		/**
 		 * @param args
 		 */
 
 	}
 
+	public static int gcd(int a, int b) {
+		return 0;		
+		
+	}
 }
