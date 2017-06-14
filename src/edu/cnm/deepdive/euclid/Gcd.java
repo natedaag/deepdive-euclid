92da8bcf742091c6c0d90285104b559ce301ac29
natedaag <natedaag@natedaag-Satellite-L755>
2017-06-14 09:19:51 -0600

    created class, parsed integers

---
+++ src/edu/cnm/deepdive/euclid/Gcd.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.euclid;
+
+/**
+ * @author natedaag
+ *
+ */
+public class Gcd {
+	
+	public static void main(String[] args) {
+	
+int a = Integer.parseInt(args[0]);
+int b = Integer.parseInt(args[1]);
+
+	/**
+	 * @param args
+	 */
+	System.out.printf("a=%d, b=%d %n", a, b);
+		// TODO Auto-generated method stub
+
+	}
+
+}
